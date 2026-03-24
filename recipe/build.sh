6867ab4050f61a8a7c2c3e49b6518a44adca1258
UENO, M. <eunosium-1128@live.jp>
2026-03-24 17:39:46 +0900

    Modify Python package find command in build script
    Update find_package for Python in CMakeLists.txt.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ if [[ "$target_platform" == "osx-"* ]]; then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
+sed -i 's|find_package(Python 3.13|find_package(Python 3.13|' python-module/CMakeLists.txt
+
 # Refer to https://github.com/conda-forge/dssp-feedstock/pull/14#issuecomment-2974049079 for `-DCIFPP_DATA_DIR=''`
 cmake -S . -B build \
     ${CMAKE_ARGS} \
